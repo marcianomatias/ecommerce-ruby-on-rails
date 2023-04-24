6629552fd5a3904a6c857aecf992ca2bbe2068f9
marcianomatias <marcianomatias1@hotmail.com>
2023-04-24 12:26:09 -0300

    :sparkles: feat: configuração devise!

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth/v1/user'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  namespace :admin do
+    namespace :v1 do
+      get "home" => "home#index"
+    end
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :storefront do
+    namespace :v1 do
+  end
+end
 end
